e16a64ed43368f808863c7790f253ec3e28532ca
nolancurtis <nolancurtis2013@gmail.com>
2026-07-13 10:09:24 -0700

    edited .h file

--- Color_Space_Conversion/LESSON_103_CSC/C/CSC_global.h
+++ Color_Space_Conversion/LESSON_103_CSC/C/CSC_global.h
@@ -24,7 +24,7 @@
 //     1 for CSC_RGB_to_YCC_brute_force_float()
 //     2 for CSC_RGB_to_YCC_brute_force_int()
 //     3 for CSC_RGB_to_YCC_optimized()
-#define RGB_to_YCC_ROUTINE 3
+#define RGB_to_YCC_ROUTINE 1
 
 // YCC_to_RGB_ROUTINE
 //     1 for CSC_YCC_to_RGB_brute_force_float()
@@ -36,7 +36,7 @@
 //     0 for returning zero (no chrominance)
 //     1 for discarding three pixels and keeping one
 //     2 for averaging four pixels
-#define CHROMINANCE_DOWNSAMPLING_MODE 2
+#define CHROMINANCE_DOWNSAMPLING_MODE 1
 
 // CHROMINANCE_UPSAMPLING_MODE = 
 //     0 for returning zero (no chrominance)
